7be56637f0fa5e1d534f90e4c0af65c8e35a85b1
itsmears0856 <vertoxstudio0856@gmail.com>
2025-04-12 15:50:46 +0530

    Update WelcomePlugin.java

--- src/me/vertox/welcome/WelcomePlugin.java
+++ src/me/vertox/welcome/WelcomePlugin.java
@@ -1,1 +1,29 @@
+package me.vertox.welcome;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.entity.Player;
+
+public class WelcomePlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("WelcomePlugin is enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("WelcomePlugin is disabled!");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.sendTitle(ChatColor.GREEN + "Welcome!", ChatColor.YELLOW + "Enjoy your stay!", 10, 70, 20);
+    }
+          }
